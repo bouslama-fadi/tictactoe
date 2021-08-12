ca1c3719b4c5dda2b03bf84395b2ca1f7dc95664
bouslama-fadi <fedy.bouslama@gmail.com>
2021-08-12 12:16:00 +0100

    initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helper";
+import Board from "./Board";
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // return if won or occupied
+    if (winner || squares[i]) return;
+    // select square
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move #${move}` : "Go to Start";
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
+
+  return (
+    <>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info-wrapper">
+        <h3 style={{ color: "red" }}>
+          {winner == "X"
+            ? "Winner: Player1"
+            : winner == "O"
+            ? "Winner: Player2"
+            : "Winner : NONE"}
+        </h3>
+        <h3 style={{ color: "blue" }}>
+          {xO == "X" ? "Next PLayer : Player1" : "Next PLayer : Player2"}
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default Game;
